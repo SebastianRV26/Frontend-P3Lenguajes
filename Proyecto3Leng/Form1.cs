44c907e78562a8c9d0255bd30b96cf245e138328
Sebastian <sebasrv26@estudiantec.cr>
2020-11-16 22:45:52 -0600

    Guardar cambios

--- Proyecto3Leng/Form1.cs
+++ Proyecto3Leng/Form1.cs
@@ -50,15 +50,15 @@ namespace Proyecto3Leng
             flag = true;
             lblStatus.Text = "Llenando matriz";
 
-            PlQuery query = new PlQuery("max(3,2,X)."); // se hace la consulta
+            //PlQuery query = new PlQuery("start."); // se hace la consulta
 
-            foreach (PlQueryVariables z in query.SolutionVariables)
+            /*foreach (PlQueryVariables z in query.SolutionVariables)
             {
                 Console.WriteLine(z["X"].ToString());
                 MessageBox.Show("X " + z["X"].ToString(), "Print", MessageBoxButtons.OKCancel);
-            }
-            query.NextSolution();
-            query.Dispose(); // se cierra la conexión
+            }*/
+            //query.NextSolution();
+            //query.Dispose(); // se cierra la conexión
             try
             {
                 int len = int.Parse(tbMatriz.Text);
@@ -80,6 +80,9 @@ namespace Proyecto3Leng
         /// </param>
         private void fillDataGridView(int tamannio)
         {
+            //dt.Rows.Clear();
+            //dt.Columns.Clear();
+            eliminarTodo();
             dt = new DataTable();
             dt.Columns.Add(" ");
             for (int i = 1; i <= tamannio; i++)
@@ -113,18 +116,41 @@ namespace Proyecto3Leng
                         if (cell.Value == "O")
                         {
                             cell.Value = " ";
+                            eliminarPunto(x, y);
                         }
                         else
                         {
                             cell.Value = "O";
+                            guardarPunto(x, y);
+                            // MessageBox.Show(x + " y " + y, "Print", MessageBoxButtons.OKCancel);
                         }
                     }
                 }
                 else
                 {
                     string message = "";
+
+                    /*PlQuery query = new PlQuery("punto(" + x + "," + y + ")."); // se hace la consulta
+                    if (query.NextSolution() == true)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                    query.Dispose();*/
+                    if (exite_punto(x, y))
+                    {
+                        message = "El punto seleccionado se encuentra en un grupo de tamaño ";
+                    } else
+                    {
+                        message = "¡El punto no se encuentra en ningún grupo!";
+                    }
+
+                    /*
                     // calcultar el tamaño del grupo al que pertenece el punto
-                    if (x == "0")
+                    if (cell.Value == "O")
                     {
                         // llamo a consulta
                         message = "El punto seleccionado se encuentra en un grupo de tamaño ";
@@ -132,8 +158,9 @@ namespace Proyecto3Leng
                     else
                     {
                         message = "¡El punto no se encuentra en ningún grupo!";
-                    }
+                    }*/
                     MessageBox.Show(message, "Consulta: tamaño al grupo del punto");
+                
                 }
 
 
@@ -167,6 +194,12 @@ namespace Proyecto3Leng
         {
             flag = false;
             lblStatus.Text = "Consultando";
+            PlQuery query = new PlQuery("punto(11,X)."); // se hace la consulta
+            foreach (PlQueryVariables z in query.SolutionVariables)
+            {
+                Console.WriteLine(z["X"].ToString());
+                MessageBox.Show("X " + z["X"].ToString(), "Print", MessageBoxButtons.OKCancel);
+            }
         }
 
         /// <summary>
@@ -194,9 +227,12 @@ namespace Proyecto3Leng
                 {
                     int column = rnd1.Next(1, len + 1);
                     int row = rnd1.Next(0, len);
-                    if (dt.Rows[row][column] != "0")
+                    if (dt.Rows[row][column] != "O")
                     {
                         dt.Rows[row][column] = "O";
+                        guardarPunto(column.ToString(), (row + 1).ToString());
+                        // MessageBox.Show(column.ToString() + " y "+ (row + 1).ToString(), "Print", MessageBoxButtons.OKCancel);
+
                     }
                     
                     tamannio--;
@@ -207,5 +243,36 @@ namespace Proyecto3Leng
                 MessageBox.Show("Ingrese un número Válido", "Advertencia");
             }
         }
+
+        //////////////////// consultas frecuentes prolog ////////////////////
+        
+        private void guardarPunto(string x, string y)
+        {
+            PlQuery query = new PlQuery("guardar_punto(" + x + "," + y + ")."); // se hace la consulta
+            query.NextSolution();
+            query.Dispose(); // se cierra la conexión
+        }
+
+        private void eliminarPunto(string x, string y)
+        {
+            PlQuery query = new PlQuery("eliminar_punto(" + x + "," + y + ")."); // se hace la consulta
+            query.NextSolution();
+            query.Dispose(); // se cierra la conexión
+        }
+
+        private void eliminarTodo()
+        {
+            PlQuery query = new PlQuery("eliminar_todo."); // se hace la consulta
+            query.NextSolution();
+            query.Dispose(); // se cierra la conexión
+        }
+
+        private bool exite_punto(string x, string y)
+        {
+            PlQuery query = new PlQuery("punto(" + x + "," + y + ").");
+            query.Dispose();
+            return query.NextSolution();
+        }
+
     }
 }
